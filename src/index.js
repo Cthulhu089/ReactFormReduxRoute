a1edefde4a10c7209d52d90f003da94160dc95a8
Marco Polo Vazquez Gutierrez <marco.vazquez@softtekswa.com>
2017-12-20 16:31:42 -0600

    Route example

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+class Foo extends React.Component {
+  render() {
+      return(<div>Hello!</div>);
+  }
+}
+
+class Foo1 extends React.Component {
+  render() {
+      return(<div>Good-bye!</div>);
+  }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Foo}/>
+        <Route path="/by" component={Foo1}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
